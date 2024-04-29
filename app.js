26381b29edf301543ef156279a6d41581dbc93df
Andrii Zhokh <andriizhokh@gmail.com>
2024-04-29 15:51:04 +0300

    from: 69 Creating HTML Pages - to: 75: Serving Files Statically

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const rootDir = require('./util/path');
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop');
@@ -7,12 +10,13 @@ const shopRoutes = require('./routes/shop');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir, 'public')))
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found:)</h1>')
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000);
