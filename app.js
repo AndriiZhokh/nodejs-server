a77636826a410d35aaa5ff98fa407c56f14df598
Andrii Zhokh <andriizhokh@gmail.com>
2024-06-26 13:11:08 +0300

    added a pug templating engine

--- app.js
+++ app.js
@@ -4,19 +4,24 @@ const bodyParser = require('body-parser');
 
 const rootDir = require('./util/path');
 
-const adminRoutes = require('./routes/admin')
+const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.set('view engine', 'pug');
+// setting directory path that contain views
+// by default it is 'views' and this line could be skipped
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, 'public')))
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+    res.status(404).render('404', { pageTitle: 'Not Found' });
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,15 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin.js');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    const products = adminData.products;
+    // render view with selected template engine
+    // for this file extension is not needed
+    res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' });
 });
 
 module.exports = router;
