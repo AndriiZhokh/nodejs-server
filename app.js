10ee89d70f36123c025d4b0c7cd5122610df26b8
Andrii Zhokh <andriizhokh@gmail.com>
2024-09-09 17:12:28 +0300

    12 mongodb setup and first connection to database

--- app.js
+++ app.js
@@ -4,17 +4,12 @@ const bodyParser = require('body-parser');
 
 const rootDir = require('./util/path');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
+
+const mongoConnect = require('./util/database');
 
 const app = express();
 
@@ -25,43 +20,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, 'public')));
 
 app.use(async (req, res, next) => {
-    try {
-        req.user = await User.findByPk(1);
-        next();
-    } catch(error) {
-        console.log(error);
-    }
+    // try {
+    //     req.user = await User.findByPk(1);
+    //     next();
+    // } catch(error) {
+    //     console.log(error);
+    // }
 })
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.getPageNotFound);
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-
-sequelize
-  // .sync({ force: true })
-  .sync()
-  .then(() => {
-    return User.findByPk(1);
+mongoConnect()
+  .then(client => {
+    console.log(client);
+    app.listen(3000);
   })
-  .then(user => {
-    if (!user) {
-      return User.create({ name: 'Andrii', email: 'Andrii@example.com' });
-    }
-
-    return Promise.resolve(user);
-  })
-  .then((user) => user.createCart())
-  .then(() => app.listen(3000))
-  .catch(error => {console.log(error)});
-
