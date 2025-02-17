6ba3722c127770580183739345e2bce12d292f9e
Andrii Zhokh <andriizhokh@gmail.com>
2025-02-17 20:25:30 +0200

    12 mongodb connection, add product to mongodb collection

--- util/database.js
+++ util/database.js
@@ -1,19 +1,30 @@
 const { mongodbPassword } = require('../secrets');
-
 const mongodb = require('mongodb');
-
 const MongoClient = mongodb.MongoClient;
+
+let _db;
 
 const mongoConnect = async () => {
 	try {
-		const client = await MongoClient.connect(`mongodb+srv://andriizhokh:${mongodbPassword}@cluster0.max1c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+		const client = await MongoClient.connect(`mongodb+srv://andriizhokh:${mongodbPassword}@cluster0.max1c.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0`)
 		console.log('Connected!');
 
+		_db = client.db();
 		return client;
 	} catch (error) {
 		console.log(error);
+		throw error;
 	}
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+	if (_db) {
+		return _db;
+	}
+	
+	throw 'No database found';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
 
