bc8cef27cb2eb6b2cad9d56a772019e38bb85cac
Andrii Zhokh <andriizhokh@gmail.com>
2025-03-03 20:28:51 +0200

    12 mongdb update products in product model

--- controllers/admin.js
+++ controllers/admin.js
@@ -22,57 +22,56 @@ exports.postAddProduct = async (req, res, next) => {
   }
 };
 
-// exports.getEditProduct = async (req, res, next) => {
-//   const editMode = req.query.edit === 'true' ? true : false;
-//
-//   if (!editMode) {
-//     return res.redirect('/');
-//   }
-//
-//   const prodId = req.params.productId;
-//
-//   try {
-//     const products = await req.user.getProducts({ where: { id: prodId } });
-//     const product = products[0];
-//
-//     if (!product) {
-//       return res.redirect('/');
-//     }
-//
-//     res.render('admin/edit-product', { pageTitle: 'Edit Product', path: '/admin/edit-product', editing: editMode, product: product });
-//   } catch(error) {
-//     console.log(error);
-//   }
-// };
-//
-// exports.postEditProduct = async (req, res, next) => {
-//   const prodId = req.body.productId;
-//   const updatedTitle = req.body.title;
-//   const updatedPrice = req.body.price;
-//   const updatedImageUrl = req.body.imageUrl;
-//   const updatedDescription = req.body.description;
-//
-//   try {
-//     const product = await Product.findByPk(prodId);
-//     product.title = updatedTitle;
-//     product.price = updatedPrice;
-//     product.imageUrl = updatedImageUrl;
-//     product.description = updatedDescription;
-//
-//     await product.save();
-//     console.log('UPDATED PRODUCT');
-//
-//     res.redirect('/admin/products');
-//   } catch(error) {
-//     console.log(error);
-//   }
-// };
-//
-// exports.getAdminProducts = async (req, res, next) => {
-//   const prods = await req.user.getProducts();
-//   res.render('admin/products', { pageTitle: 'Admin Products', path: '/admin/products', prods });
-// };
-//
+exports.getEditProduct = async (req, res, next) => {
+  const editMode = req.query.edit === 'true' ? true : false;
+
+  if (!editMode) {
+    return res.redirect('/');
+  }
+
+  const prodId = req.params.productId;
+
+  try {
+    const product = await Product.findById(prodId);
+
+    if (!product) {
+      return res.redirect('/');
+    }
+
+    res.render('admin/edit-product', { pageTitle: 'Edit Product', path: '/admin/edit-product', editing: editMode, product: product });
+  } catch(error) {
+    console.log(error);
+  }
+};
+
+exports.postEditProduct = async (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDescription = req.body.description;
+
+  try {
+    const product = await Product.findByPk(prodId);
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.imageUrl = updatedImageUrl;
+    product.description = updatedDescription;
+
+    await product.save();
+    console.log('UPDATED PRODUCT');
+
+    res.redirect('/admin/products');
+  } catch(error) {
+    console.log(error);
+  }
+};
+
+exports.getAdminProducts = async (req, res, next) => {
+  const prods = await Product.fetchAll();
+  res.render('admin/products', { pageTitle: 'Admin Products', path: '/admin/products', prods });
+};
+
 // exports.postDeleteProduct = async (req, res, next) => {
 //   const productId = req.body.productId;
 //
